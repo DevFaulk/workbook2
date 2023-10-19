4f75c1413ddf0eb356a61eb0e58ea894d59c3d59
Teddy Faulk <FaulkDev@gmail.com>
2023-10-19 10:18:23 -0400

    add excercises

---
+++ 1-26.js
@@ -0,0 +1,33 @@
+// excercise 1
+// var myMessage = "What's the word";
+// console.log("Message: " + myMessage);
+
+// excercise 2
+// var string1 = "string1 ";
+// var string2 = "string2";
+// var string3 = string1 + string2;
+// console.log(string3);
+
+// excercise 3
+// var customerId;
+// var firstName;
+// var middleName;
+// var lastName;
+// var gender;
+// var dateOfBirth;
+// var licenseNumber;
+// var policyNumber;
+
+// excercise 4
+// var firstName;
+// var cityName;
+// var stateName;
+
+// firstName = "Theodore";
+// cityName = "Pittsburgh";
+// stateName = "Pennsylvania";
+// console.log("My name is: " + firstName + ", from " + cityName + ", " + stateName);
+
+// excercise 5
+var number1 = 5;
+console.log(number1);
