bf7cc2c1df08c4813cd987a14bd800fce647b2ff
Teddy Faulk <FaulkDev@gmail.com>
2023-10-24 09:49:12 -0400

    add exercise

---
+++ ifscripts/show_major.js
@@ -0,0 +1,38 @@
+var bioStudentName = "Bonnie";
+var bioStudentMajor = "BIOL, Biology";
+var bioMajorLocat = "Science Bldg Room 310";
+
+var comStudentName = "Betty";
+var comStudentMajor = "CSCI, Computer Science";
+var comMajorLocat = "Sheppard Hall Room 314";
+
+var engStudentName = "Barny";
+var engStudentMajor = "ENG, English";
+var engMajorLocat = "Kerr Hall Room 201";
+
+var histStudentName = "Bunny";
+var histStudentMajor = "HIST, History";
+var histMajorLocat = "Kerr Hall Room 114";
+
+var mktStudentName = "Bobby";
+var mktStudentMajor = "MKT, Marketing";
+var mktMajorLocat = "Westly Hall Room 310";
+
+//   enter name here to search
+var studentName = "Barny";
+
+if (studentName == bioStudentName) {
+  console.log("Student: " + bioStudentName + "|Major: " + bioStudentMajor + "|Advising Location: " + bioMajorLocat);
+} else if (studentName == engStudentName) {
+  console.log("Student: " + engStudentName + "|Major: " + engStudentMajor + "|Advising Location: " + engMajorLocat);
+} else if (studentName == histStudentName) {
+  console.log("Student: " + histStudentName + "|Major: " + histStudentMajor + "|Advising Location: " + histMajorLocat);
+} else if (studentName == comStudentName) {
+  console.log("Student: " + comStudentName + "|Major: " + comStudentMajor + "|Advising Location: " + comMajorLocat);
+} else if (studentName == engStudentName) {
+  console.log("Student: " + engStudentName + "|Major: " + engStudentMajor + "|Advising Location: " + engMajorLocat);
+} else if (studentName == histStudentName) {
+  console.log("Student: " + histStudentName + "|Major: " + histStudentMajor + "|Advising Location: " + histMajorLocat);
+} else if (studentName == mktStudentName) {
+  console.log("Student: " + mktStudentName + "|Major: " + mktStudentMajor + "|Advising Location: " + mktMajorLocat);
+}
